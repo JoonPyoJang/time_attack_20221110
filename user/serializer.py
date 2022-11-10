831029a4fb879f40677fbf81e0889b2808ec1828
JoonPyoJang <jjpyo1009@gmail.com>
2022-11-10 11:13:32 +0900

    upload test

---
+++ user/serializer.py
@@ -0,0 +1,21 @@
+from rest_framework import serializers
+from user.models import User
+from rest_framework_simplejwt.serializers import TokenObtainPairSerializer
+from rest_framework_simplejwt.views import TokenObtainPairView
+
+class UserSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = User
+        fields = "__all__"
+
+
+
+class MyTokenObtainPairSerializer(TokenObtainPairSerializer):
+    @classmethod
+    def get_token(cls, user):
+        token = super().get_token(user)
+
+        token['email'] = user.email
+        token['token_message'] = "sparta_time_attack"
+
+        return token
